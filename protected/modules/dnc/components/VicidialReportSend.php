0f3e542843496e6757c195d2ea3086c644070eb1
kebyang <kelvo_link@yahoo.com>
2015-08-12 22:30:45 +0800

    lkahsdkas

--- protected/modules/dnc/components/VicidialReportSend.php
+++ protected/modules/dnc/components/VicidialReportSend.php
@@ -8,7 +8,7 @@ class VicidialReportSend
 	public static function send($mobileNumber)
 	{
 		$mobileNumber = doubleval($mobileNumber);
-		$curlURL = "http://213.171.204.244/vicidial/non_agent_api.php?source=dncadding&user=apiuserwill&pass=mentalapipassword&function=add_mednc&dnc_check=Y&phone_number=$mobileNumber";
+		$curlURL = "http://213.171.204.244/vicidial/non_agent_api.php?source=callurldnc&user=apiuserwill&pass=mentalapipassword&function=add_mednc&dnc_check=Y&phone_number=$mobileNumber";
 		$curlres = curl_init($curlURL);
 		curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
 		return curl_exec($curlres);
