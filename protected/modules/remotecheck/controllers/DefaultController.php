99930d70282b89e46edb7c345620cb0121e332c2
kebyang <kelvo_link@yahoo.com>
2015-08-31 19:38:47 +0800

    remote blacklist checker

--- protected/modules/remotecheck/controllers/DefaultController.php
+++ protected/modules/remotecheck/controllers/DefaultController.php
@@ -6,6 +6,17 @@ class DefaultController extends Controller
 	{
 		header("Content-Type: application/json");
 		$jsonMessage =  array();
-		
+		$mobileNumber = doubleval($mobileNumber);
+		$isBlackListed = BlackListedMobile::model()->exists("mobile_number = :mobile_number", array('mobile_number'=>$mobileNumber));
+		if ($isBlackListed) {
+			$jsonMessage = array(
+				"status"=>"blacklisted",
+			);
+		}else{
+			$jsonMessage = array(
+				"status"=>"clean",
+			);
+		}
+		echo json_encode($jsonMessage);
 	}
 }
