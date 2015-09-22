a426145e17beba025d7873cea49f7293bcb0f414
kebyang <kelvo_link@yahoo.com>
2015-09-22 19:07:44 +0800

    debug mode

--- protected/modules/redirect/controllers/DataController.php
+++ protected/modules/redirect/controllers/DataController.php
@@ -6,7 +6,7 @@ class DataController extends CController
 {
 	public function actionIndex()
 	{
-		header("Content-Type: application/json");
+		// header("Content-Type: application/json");
 		$allowedDispo = array("FISH","SCR","DNC","OPTOUT","5press");
 		$jsonMessage = array();
 		if ( (  isset($_GET['dispo']) && !empty($_GET['dispo'])  )  && (  isset($_GET['phone_number']) && !empty($_GET['phone_number'])  ) ) {
