8c8a13c94b1c1181298620d4e81385d8447a07cb
kebyang <kelvo_link@yahoo.com>
2015-09-25 21:15:19 +0800

    ignore ssl

--- protected/modules/redirect/components/NewViciCopySource.php
+++ protected/modules/redirect/components/NewViciCopySource.php
@@ -19,7 +19,10 @@ class NewViciCopySource
 		$curlURL = $url.http_build_query($httpParameters);
 		$curlres = curl_init($curlURL);
 		curl_setopt($curlres, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlres, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curlres, CURLOPT_SSL_VERIFYPEER, false);
 		$curlResRaw = curl_exec($curlres);
+		Yii::log($curlURL, CLogger::LEVEL_INFO,'info');
 		Yii::log($curlResRaw, CLogger::LEVEL_INFO,'info');
 	}
 }
