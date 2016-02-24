44b8efc3d6a078cf6747e57d22a6b33534dba3f8
Your Name <you@example.com>
2016-02-25 04:01:18 +0800

    dsa

--- protected/modules/redirect/controllers/DataController.php
+++ protected/modules/redirect/controllers/DataController.php
@@ -108,7 +108,7 @@ class DataController extends CController
 				$mail->IsSMTP(); // enable SMTP
 				$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 				$mail->SMTPAuth = true; // authentication enabled
-				$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+				$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
 				$mail->Host = "smtp.gmail.com";
 				$mail->Port = 465; // or 587
 				$mail->IsHTML(true);
