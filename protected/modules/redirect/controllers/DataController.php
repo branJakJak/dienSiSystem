9dff1191b3d15718f3daa01a6a4b133b4c22740d
Your Name <you@example.com>
2016-02-25 03:54:43 +0800

    dsa

--- protected/modules/redirect/controllers/DataController.php
+++ protected/modules/redirect/controllers/DataController.php
@@ -104,23 +104,31 @@ class DataController extends CController
 			if (in_array($status, $sendToSpreadsheetDispo)) {
 				//if in array of send to spreadsheet
 				//send mail
-				$mailer = new YiiMailer();
-				$mailer->setFrom("infoConsultancyTeam@gmail.com");
-				$mailer->setSubject("5PF and DELAY8");
-				$mailer->setTo("infoConsultancyTeam@gmail.com");
-				$mailer->setBody($phone_number);
-				$mailer->send();
-				if ($mailer->send()) {
+				$mail = new PHPMailer(); // create a new object
+				$mail->IsSMTP(); // enable SMTP
+				$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+				$mail->SMTPAuth = true; // authentication enabled
+				$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+				$mail->Host = "smtp.gmail.com";
+				$mail->Port = 465; // or 587
+				$mail->IsHTML(true);
+				$mail->Username = "infoConsultancyTeam@gmail.com";
+				$mail->Password = "Newuser123!";
+				$mail->SetFrom("infoConsultancyTeam@gmail.com");
+				$mail->Subject = "5PF and DELAY8";
+				$mail->Body = $phone_number;
+				$mail->AddAddress("infoConsultancyTeam@gmail.com");
+				 if(!$mail->Send()) {
+					$jsonMessage = array(
+						"status"=>"failed",
+						"description"=>$mail->ErrorInfo,
+					);
+				 } else {
 					$jsonMessage = array(
 						"status"=>"ok",
 						"description"=>"Phone number sent.",
-					);
-				}else{
-					$jsonMessage = array(
-						"status"=>"failed",
-						"description"=>$mailer->getError(),
-					);
-				}
+					);				 	
+				 }
 			}
 		}//end of if requrest is vaild
 		else{
