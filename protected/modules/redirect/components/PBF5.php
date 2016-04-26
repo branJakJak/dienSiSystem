bfe3401fae8c4b75c20f3b07160586e599e10589
Your Name <you@example.com>
2016-04-26 19:14:41 +0800

    remove zero

--- protected/modules/redirect/components/PBF5.php
+++ protected/modules/redirect/components/PBF5.php
@@ -14,13 +14,14 @@ class PBF5 extends RemoteViciOutBase
             "function"=>"add_lead",
             "phone_number"=>$this->getPhoneNumber(),
             "phone_code"=>"44",
-            "list_id"=>"0770",
+            "list_id"=>"770",
             // "dnc_check"=>"Y",
             // "duplicate_check"=>"DUPLIVE",
             "add_to_hopper"=>"Y",
             "hopper_priority"=>"45"
         );
         $httpParameters = array_merge($httpParameters, $this->getAdditionalParameters());
+        $httpParameters['list_id'] = intval($httpParameters['list_id']);
         $res = $this->sendToRemoteServer($httpParameters);
         $jsonMessage['vici_res'] = $res;
         return $jsonMessage;
--- protected/modules/redirect/components/PIF5.php
+++ protected/modules/redirect/components/PIF5.php
@@ -14,13 +14,14 @@ class PIF5 extends RemoteViciOutBase
             "function"=>"add_lead",
             "phone_number"=>$this->getPhoneNumber(),
             "phone_code"=>"44",
-            "list_id"=>"0550",
+            "list_id"=>"550",
             // "dnc_check"=>"Y",
             // "duplicate_check"=>"DUPLIVE",
             "add_to_hopper"=>"Y",
             "hopper_priority"=>"45"
         );
         $httpParameters = array_merge($httpParameters, $this->getAdditionalParameters());
+        $httpParameters['list_id'] = intval($httpParameters['list_id']);
         $res = $this->sendToRemoteServer($httpParameters);
         $jsonMessage['vici_res'] = $res;
         return $jsonMessage;
