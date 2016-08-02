0bedf052683bd7b268e87468a97c00d897e9ceef
Your Name <you@example.com>
2016-08-02 16:56:39 +0800

    prod

--- protected/yiic.php
+++ protected/yiic.php
@@ -2,7 +2,7 @@
 
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
-// $config=dirname(__FILE__).'/config/console.php';
-$config=dirname(__FILE__).'/config/console_dev.php';
+$config=dirname(__FILE__).'/config/console.php';
+// $config=dirname(__FILE__).'/config/console_dev.php';
 
 require_once($yiic);
