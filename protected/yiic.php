3a5d547488d775a065684973389f048c6bc5d6f1
kevin daus <kevinflorenzdaus@gmail.com>
2016-09-15 03:34:33 +0800

    yiic prod mode

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,9 +1,9 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+// defined('YII_DEBUG') or define('YII_DEBUG',true);
+// defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-defined('LOCAL_MODE') or define('LOCAL_MODE',true);
+// defined('LOCAL_MODE') or define('LOCAL_MODE',true);
 
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
