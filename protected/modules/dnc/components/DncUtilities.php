f6945298ddc1caaa300f3807b8967861a617698b
kevin daus <kevinflorenzdaus@gmail.com>
2016-09-15 03:47:51 +0800

    sed "s/'/\'/;s/\t/\","/g;s/^/\"/;s/$/\"/;s/\n//g"

--- protected/modules/dnc/components/DncUtilities.php
+++ protected/modules/dnc/components/DncUtilities.php
@@ -165,7 +165,8 @@ OFFSET ' . $offset . '
             /**
              * Write the result in $exportFileLocation
              */
-            $mainCommand = "nohup mysql  --user=dncsyste_dnc --password=hitman052529 --database=dncsyste_dnc -e '$sqlCommand' | sed \"s/'/\\'/;s/\\t/\\\",\"/g;s/^/\\\"/;s/$/\\\"/;s/\\n//g\" > $exportFileLocation 2>&1 &";
+            $mainCommand = "nohup mysql  --user=dncsyste_dnc --password=hitman052529 --database=dncsyste_dnc -e '$sqlCommand' | sed \"s/'/\\'/;s/\\t/\\\",\\\"/g;s/^/\\\"/;s/$/\\\"/;s/\\n//g\" > $exportFileLocation 2>&1 &";
+
             exec($mainCommand);
             /*remove duplicate lines*/
             //$exportFileLocation
