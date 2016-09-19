8e8daab4181e582cd9f9183e69d24550dc8b2220
kevin daus <kevinflorenzdaus@gmail.com>
2016-09-19 18:36:12 +0800

    fixing .gitignore

--- protected/views/mail/contact.php
+++
@@ -1,4 +0,0 @@
-<h1>One of our site went down : 
-	<a href="<?php echo $websiteURL ?>">
-	<?php echo $accountName ?>
-</a></h1>
--- protected/yiic.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-// $config=dirname(__FILE__).'/config/console_dev.php';
-
-require_once($yiic);
