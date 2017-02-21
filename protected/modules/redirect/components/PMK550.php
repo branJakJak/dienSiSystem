4f70e04a5a0c4a662c4f7e784434dd40bd688cdf
kevin daus <kevinflorenzdaus@gmail.com>
2017-02-21 17:09:47 +0800

    added PMK550

---
+++ protected/modules/redirect/components/PMK550.php
@@ -0,0 +1,28 @@
+<?php 
+
+/**
+* PMK550
+*/
+class PMK550 extends RemoteViciOutBase
+{
+    public function send()
+    {
+        $httpParameters = array(
+            "source"=>"PMK550",
+            "user"=>"apiuserwill",
+            "pass"=>"mentalapipassword",
+            "function"=>"add_lead",
+            "phone_number"=>$this->getPhoneNumber(),
+            "phone_code"=>"44",
+            "list_id"=>"551",
+            "dnc_check"=>"Y",
+            "duplicate_check"=>"DUPLIVE",
+            "add_to_hopper"=>"Y",
+            "hopper_priority"=>"45"
+        );
+        $httpParameters = array_merge($this->getAdditionalParameters() , $httpParameters);
+        $res = $this->sendToRemoteServer($httpParameters);
+        $jsonMessage['vici_res'] = $res;
+        return $jsonMessage;
+    }
+}
