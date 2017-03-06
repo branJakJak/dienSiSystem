b3fc4ff995b7f8feac7f746f08d6454a0bf96e7b
kevin daus <kevinflorenzdaus@gmail.com>
2017-03-07 03:58:40 +0800

    test control

--- protected/config/dev/main.php
+++ protected/config/dev/main.php
@@ -41,6 +41,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'password'=>'password'
 			// 'ipFilters'=>array('*.*.*.*'),
 		),
 	),
